0cc42540b0fddefca6a66cb96e6c82b1432fda6a
Harshit058 <harshitshukla058@gmail.com>
2025-09-08 11:27:27 +0530

    Refactoring Code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDAoI-8gADbz3o3UD_2TQzlravG21SU-7g",
   authDomain: "fanmania-6c649.firebaseapp.com",
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { ChakraProvider } from '@chakra-ui/react';
-// import { extendTheme } from '@chakra-ui/react';
-// import {mode} from '@chakra-ui/theme-tools';
-// import { BrowserRouter } from 'react-router-dom';
-
-// const styles={
-//   global:(props) => ({
-//     body:{
-//       bg:mode("gray.100", "#000")(props),
-//       color:mode("gray.800","whiteAlpha.900")(props),
-//     }
-//   })
-// }
-// const config = {
-//   initialColorMode: 'dark',
-//   useSystemColorMode: false,
-// }
-
-// const theme = extendTheme({ config, styles});
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <ChakraProvider theme={theme}>
-//     <App />
-//     </ChakraProvider>
-//     </BrowserRouter>
-    
-    
-//   </React.StrictMode>,
-// )
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -47,7 +11,7 @@ const styles = {
     body: {
       bg: '#EDEDE9', // Set the background color to #EDEDE9
       color: 'gray.800', // Optionally, specify the text color
-     
+
     },
   },
 };
